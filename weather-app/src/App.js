ebee856dd1207d4d22e91187f5fbd64ba34b40e5
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-30 14:57:40 -0800

    Setup React
    Set up the environment

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Bare Bones app.</p>
+    </div>
+  );
+}
+
+export default App;

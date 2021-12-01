7b46d72c7a73e887ca56cb78772328fca3f11495
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-30 17:27:19 -0800

    Started working on React App
    On video 2

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Bare Bones app.</p>
+      <Info/>
+      <AddItem/>
     </div>
   );
+}
+
+function Info(){
+  const title = "this is my title";
+  const showTitle = true;
+  return (
+    <div>
+      <h1>{showTitle ? title: ""}</h1>
+      <p>Manage your stuff.</p>
+    </div>
+  )
+}
+
+function AddItem() {
+  return (
+    <div>
+      <form>
+        <label for="text-form">Type something</label>
+        <input type="text" id="text-form"/>
+      </form>
+    </div>
+  )
 }
 
 export default App;

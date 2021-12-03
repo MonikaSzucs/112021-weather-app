1bfaf8bbd3b586d75473688fb557088c6eb20f2e
Monika <monika.silvia.s@hotmail.ca>
2021-12-02 16:56:36 -0800

    Started Navbar
    https://www.youtube.com/watch?v=VzWBLj_CfpE

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,10 @@
 import './styles/App.css';
+import Header from './Components/Navbar/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Info/>
       <AddItem/>
     </div>
